b99d0d445965f66dc01625abc9e64d80b898e462
Marc CB <marc.cbrooks@gmail.com>
2018-02-10 10:49:16 -0500

    finish getTrades func

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"net/http"
 	"sync"
 )
@@ -25,11 +27,26 @@ func main() {
 
 }
 
-func getTrades(pair string, trade chan<- Currency, wg *sync.WaitGroup) {
+func getTrades(pair string, tradeCH chan<- Currency, wg *sync.WaitGroup) {
+	slice := []GDAXTrade{}
+
 	api := "https://api.gdax.com/products/" + pair + "/trades?limit=1"
 	resp, err := http.Get(api)
 	if err != nil {
-
+		tradeCH <- Currency{pair, "-----"}
+		wg.Done()
 	}
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		tradeCH <- Currency{pair, "-----"}
+		wg.Done()
+	}
+	err = json.Unmarshal(bytes, &slice)
+	if err != nil {
+		tradeCH <- Currency{pair, "-----"}
+		wg.Done()
+	}
+	tradeCH <- Currency{pair, slice[0].Price}
 	wg.Done()
 }
--- types.go
+++ types.go
@@ -1,10 +1,5 @@
 package main
 
-// Response is an array of length 1 that contains the most recent trade for a cryptocurrency
-type Response struct {
-	Trades [1]GDAXTrade
-}
-
 // GDAXTrade contains data for a single trade
 type GDAXTrade struct {
 	Time    string `json:"time"`
