8750c690d676fb9a5ac8b274eae5384fe2083f90
Marc CB <marc.cbrooks@gmail.com>
2018-02-10 15:23:26 -0500

    add new types

--- types.go
+++ types.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 // GDAXTrade contains data for a single trade
 type GDAXTrade struct {
 	Time    string `json:"time"`
@@ -13,4 +15,25 @@ type GDAXTrade struct {
 type Currency struct {
 	Name  string
 	Trade GDAXTrade
+}
+
+// Subscribe is the structure for the subscription message sent to GDAX websocket API
+type Subscribe struct {
+	Type       string   `json:"type"`
+	ProductIds []string `json:"product_ids"`
+	Channels   []string `json:"channels"`
+}
+
+// Match contains structure for websocket messages from matches channel
+type Match struct {
+	Type         string    `json:"type"`
+	TradeID      int       `json:"trade_id"`
+	Sequence     int       `json:"sequence"`
+	MakerOrderID string    `json:"maker_order_id"`
+	TakerOrderID string    `json:"taker_order_id"`
+	Time         time.Time `json:"time"`
+	ProductID    string    `json:"product_id"`
+	Size         string    `json:"size"`
+	Price        string    `json:"price"`
+	Side         string    `json:"side"`
 }
